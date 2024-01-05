23b9c3fb7940a0e6e456d3bdaffa327e9eb71eea
Abhishikta Ray <127621443+Abhishikta03@users.noreply.github.com>
2024-01-05 23:14:50 +0530

    let's commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let meme = document.getElementById("meme");
+let title = document.getElementById("heading");
+let getMemeBtn = document.getElementById("generate");
+//API URL
+let url="https://meme-api.com/gimme/";
+//subreddits
+let subreddits= ["catmemes","dogmemes","wholesomemes","me_irl"];
+
+//FUNCTION TO GET RANDOM MEME
+let getMeme =() =>{
+    let randomsubreddit=subreddits[Math.floor(Math.random()*subreddits.length)];
+
+    //FETCH DATA FROM API
+    fetch(url+randomsubreddit).then((resp) => resp.json()).then((data)=> {
+        let memeImg=new Image();
+        //Display meme image and title only after the image loads
+        memeImg.onload = () =>{
+            meme.src = data.url;
+            title.innerHTML = data.title;
+        };
+        memeImg.src =  data.url;
+    });
+    
+};
+
+getMemeBtn.addEventListener("click",getMeme);
+window.addEventListener("load",getMeme);
